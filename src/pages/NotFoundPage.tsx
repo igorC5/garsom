ce8818790ef38ac24743d089af41711c320ab1ea
igorC5 <igorcandidofs@gmail.com>
2025-04-26 11:25:08 -0300

    docs:

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,8 +1,6 @@
 import { Link } from "react-router-dom";
 
-
 export default function NotFoundPage(){
-
     return (
         <div className="bg-background w-[100%] h-[100vh] flex justify-center items-center">
             <div className="bg-white border-4 shadow-[10px_10px_0px_0px_black] border-solid absolute top-25 h-[20%] w-[20%] flex justity-center items-center">
